a56b374a0c5093ac257792229f3b4b507885a4dd
riz7861 <razwan_mo@hotmail.co.uk>
2023-10-25 15:30:46 +0100

    Modifided the scripts to allow keydown as well as pop for reset score

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # rock-paper-scissors
 
 Project to make a basic game using Javascript
+
+The below keys also allow the game to be run;
+
+R for Rock.
+
+P for Paper.
+
+S for Scissors.
+
+A will activate the auto play function.
+
+Backspace allows the scores to be reset.
--- script.js
+++ script.js
@@ -8,22 +8,82 @@ updateScoreElement ();
 
 isautoplaying = false;
 
-let intervalid; 
+let intervalid;
 
-function autoplay () {
-    if (!isautoplaying) {
-        intervalid = setInterval(function() {
-            const playerMove = pickComputerMove ();
-            playGame(playerMove);
-        }, 1000);
-        isautoplaying = true;
-    } else {
-        clearInterval(intervalid);
-        isautoplaying = true;
+let button = document.querySelector(".js-auto-play");
 
-    }
-    
+if (button) {
+    button.addEventListener("click,", autoplay);
+} else {
+    console.error("Button with class .js-auto-play not found!");
 }
+function autoplay() {
+    let button = document.querySelector(".js-auto-play");
+
+    if (button) {
+        if (!isautoplaying) {
+            button.innerHTML = 'Active';
+
+            intervalid = setInterval(() => {
+                const playerMove = pickComputerMove();
+                playGame(playerMove);
+            }, 1000);
+            isautoplaying = true;
+        } else {
+            button.innerHTML = 'Auto Play'; // or whatever the original text was
+
+            clearInterval(intervalid);
+            isautoplaying = false;
+        }
+    } else {
+        console.error("Button with class .js-auto-play not found!");
+    }
+}
+
+
+
+document.querySelector('.js-auto-play')
+    addEventListener('click', () => {
+        autoplay();
+    });
+
+document.querySelector('.js-rock-button')
+    addEventListener('click', () => {
+        playGame ('Rock');
+    });
+
+document.querySelector('.js-paper-button')
+    addEventListener('click', () => {
+        playGame ('Paper');
+    });
+
+document.querySelector('.js-scissors-button')
+    addEventListener('click', () => {
+        playGame ('Scissors');
+    });
+
+document.body,addEventListener ('keydown', (event) => {
+    if (event.key === 'r') {
+        playGame ('Rock'); 
+    }
+    else if (event.key === 'p') {
+        playGame ('Paper');
+    }    
+    else if (event.key === 's') {
+            playGame ('Scissors');
+    }
+    else if (event.key === 'a') {
+        autoplay();
+    }
+    else if (event.key === 'Backspace') {
+        resetScoreAndLocalStorage();
+    }
+
+});
+
+document.querySelector('.js-reset').addEventListener('click', resetScoreAndLocalStorage);
+
+
 
 function playGame (playerMove) {
     const computerMove = pickComputerMove();
@@ -105,4 +165,19 @@ function pickComputerMove () {
     }
 
     return computerMove; 
+}
+
+function resetScoreAndLocalStorage() {
+    let isConfirmed = window.confirm("Are you sure you want to reset the score?");
+    
+    if (isConfirmed) {
+        score.wins = 0;
+        score.draws = 0;
+        score.losses = 0;
+        localStorage.removeItem('score');
+        updateScoreElement();
+        alert('Score reset successfully!');
+    } else {
+        alert('Score reset canceled.');
+    }
 }
